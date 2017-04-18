a01a6c57219656edd293cc21dd16dc3fd6b78493
Optimove Mobile <optimove.develop.mobile@gmail.com>
2017-04-18 20:20:38 +0300

    V1.2

--- ClientFlow/clientAPI/api.js
+++ ClientFlow/clientAPI/api.js
@@ -1,18 +1,45 @@
- 
   module.exports = function api(options) {
 
   var valid_ops = { sum:'sum', product:'product' }
 
   this.add('role:api,path:calculate', function (msg, respond) {
-    var operation = msg.args.params.operation
-    var left = msg.args.query.left
-    var right = msg.args.query.right
+
+    var left = msg.left;
+    var right = msg.right;
     this.act('role:math', {
-      cmd:   valid_ops[operation],
+      cmd:   'sum',
       left:  left,
       right: right,
     }, respond)
   })
+
+
+
+ this.add('role:api,path:campaignapi', function (msg, respond) {
+
+      var command_name =    msg.command_name;
+      var campaign_mode =   msg.campaign_mode;
+      var target_types =    msg.target_types;
+      var tenant_id =       msg.tenant_id;
+      var campaign_id =     msg.campaign_id;
+      var action_serial =   msg.action_serial;
+      var num_tgt_devices = msg.num_tgt_devices;
+      var schedule =        msg.schedule;
+      var time_to_live =    msg.time_to_live;
+
+      this.act('role:campaignapi', {
+          cmd:              'create',
+          command_name:         command_name,
+          tenant_id:            tenant_id,
+          campaign_id:      campaign_id,
+          action_serial:        action_serial,
+          num_tgt_devices:  num_tgt_devices,
+          topic_name:       topic_name,
+          schedule:             schedule,
+          time_to_live:     time_to_live
+      }, respond)
+  })
+
 
 
   this.add('init:api', function (msg, respond) {
