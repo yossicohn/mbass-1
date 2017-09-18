73ece509cadb8fc5186af348b9799b6f0e861308
yossi_c <yossi_c@optimove.com>
2017-09-18 16:51:40 +0300

    add folder

---
+++ client-flow/client-campaign-api/client-campaign-service/routes/functions-routes.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = function(app) {
+
+
+    var functions_api = require('../functions/functions-api');
+
+    //  Routes
+    app.route('/register-visitor')
+    .post(functions_api.register_visitor);
+    //  Routes
+    app.route('/unregister-visitor')
+    .post(functions_api.unregister_visitor);
+
+    //  Routes
+    app.route('/register-customer')
+        .post(functions_api.register_customer);
+        //  Routes
+    app.route('/unregister-customer')
+    .post(functions_api.unregister_customer);
+
+    app.route('/optinout-customer')
+    .post(functions_api.opt_in_out_customer);
+
+    app.route('/optinout-visitor')
+    .post(functions_api.opt_in_out_visitor);
+
+}
+
