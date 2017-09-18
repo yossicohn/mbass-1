cce7d8a9a00d08ca52bf33846b4c1f420f411420
yossi_c <yossi_c@optimove.com>
2017-09-18 16:51:17 +0300

    fixes optinout

--- registration-flow/registration-service-testing/routes/functions-routes.js
+++ registration-flow/registration-service-testing/routes/functions-routes.js
@@ -6,23 +6,23 @@ module.exports = function(app) {
 
     //  Routes
     app.route('/register-visitor')
-    .post(functions_api.register_visitor);
+    .post(functions_api.registerVisitor);
     //  Routes
     app.route('/unregister-visitor')
-    .post(functions_api.unregister_visitor);
+    .post(functions_api.unregisterVisitor);
 
     //  Routes
     app.route('/register-customer')
-        .post(functions_api.register_customer);
+        .post(functions_api.registerCustomer);
         //  Routes
     app.route('/unregister-customer')
-    .post(functions_api.unregister_customer);
+    .post(functions_api.unregisterCustomer);
 
     app.route('/optinout-customer')
-    .post(functions_api.opt_in_out_customer);
+    .post(functions_api.optInOutCustomer);
 
     app.route('/optinout-visitor')
-    .post(functions_api.opt_in_out_visitor);
+    .post(functions_api.optInOutVisitor);
 
 }
 
